008fa951554a3c5a65e915f1ab342d4efac98a23
David Jeremias Souza do Carmo <p577394@corp.caixa.gov.br>
2019-03-27 18:39:37 -0300

    criação do usuario

---
+++ mirante-backend/src/main/java/com/u2d/erp/com/u2d/erp/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.u2d.erp.com.u2d.erp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.u2d.erp.com.u2d.erp.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
