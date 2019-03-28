33820e9272b4ca54affe067c220ca2600bce1544
David Jeremias Souza do Carmo <p577394@corp.caixa.gov.br>
2019-03-28 10:12:21 -0300

    validações backend bean validations

--- mirante-backend/src/main/java/com/u2d/erp/com/u2d/erp/model/Cliente.java
+++ mirante-backend/src/main/java/com/u2d/erp/com/u2d/erp/model/Cliente.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import java.util.List;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -14,16 +17,25 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull
     private String cpf;
+    
+    @NotNull
+    @Size(min = 3, max = 100)
     private String nome;
+    
+    @NotNull
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id")
     private Endereco endereco;
     
+    @NotEmpty
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name= "cliente_id")
     private List<Telefone> telefones;
     
+    @NotEmpty
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "cliente_id")
     private List<Email> emails;
--- mirante-backend/src/main/java/com/u2d/erp/com/u2d/erp/model/Endereco.java
+++ mirante-backend/src/main/java/com/u2d/erp/com/u2d/erp/model/Endereco.java
@@ -3,6 +3,7 @@ package com.u2d.erp.com.u2d.erp.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -12,10 +13,21 @@ public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull
     private String cep;
+    
+    @NotNull
     private String logradouro;
+    
+    @NotNull
     private String bairro;
+    
+    @NotNull
     private String cidade;
+    
+    @NotNull
     private String uf;
+    
     private String complemento;
 }
